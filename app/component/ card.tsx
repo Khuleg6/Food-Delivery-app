e44f854fdbd1fff29d69081e240e106447faf085
Khuleg <26bs38301nest.edu.mn@G301-CC0210.local>
2026-05-19 20:00:18 +0800

    db

---
+++ app/component/ card.tsx
@@ -0,0 +1,14 @@
+export const Card = () => {
+  return (
+    <div>
+      <div className="bg-[url('/proimg.png')] bg-cover bg-center w-[397px]">
+        <button></button>
+      </div>
+      <div>
+        <div>   <p>Sunshine Stackers</p>
+        <p></p></div>
+
+      </div>
+    </div>
+  );
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { Navigation } from "./component/navigation";
 import { Footer } from "./component/footer";
+import { Card } from "./component/ card";
 
 export default function Home() {
   return (
@@ -11,9 +12,9 @@ export default function Home() {
       </div>
       <div className="h-[960px]">
         <p>Appetizers</p>
+        <Card />
       </div>
       <div>
-        {" "}
         <Footer />
       </div>
     </div>
